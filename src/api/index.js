c7181cf9d353d2afc0da7a96e73896aa5ed1a534
Temitayo Ogunsusi <ogunsusitemitayo99@gmail.com>
2022-03-17 18:35:59 +0100

    Removed unnecessary environment variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_URL || 'http://178.63.13.157:8090/mock-api/api',
+  baseURL: 'http://178.63.13.157:8090/mock-api/api',
   headers: {
     'Content-Type': 'application/json',
   },
